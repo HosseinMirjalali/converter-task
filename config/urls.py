52ed833c79790361e89872a2dd2440c4f4b0425a
hossein <mirjalali.hosein@gmail.com>
2021-12-26 19:47:02 +0330

    added drf_yasg dependency for swagger and basic configuration

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,28 @@
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.authtoken.views import obtain_auth_token
+
+# Required schema configurations for drf_yasg
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Video Converter API",
+        default_version="v1",
+        description="Video converter task",
+        terms_of_service="https://www.convertertask.ir/policies/terms/",
+        contact=openapi.Contact(email="contact@convertertask.ir"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
@@ -17,6 +35,19 @@ urlpatterns = [
     path("users/", include("converter.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    url(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    url(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    url(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # API URLS
