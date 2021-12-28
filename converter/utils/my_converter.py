71a5415878e5ea28d88a1898c9b868e150dae56a
hossein <mirjalali.hosein@gmail.com>
2021-12-29 00:01:15 +0330

    updates to the converter function, now the old file is deleted, new file in proper dir

--- converter/utils/my_converter.py
+++ converter/utils/my_converter.py
@@ -1,18 +1,32 @@
+import os
+import shutil
+from django.conf import settings
 import ffmpeg
 
 from converter.video.models import VideoRaw, VideoConverted
 
 
 def convert(raw_uuid, conv_uuid):
+    """
+    A simple converter function that uses ffmpeg wrapper package to execute ffmpeg commands.
+    First extracts the given objects' (see params) information from database,
+    then makes an output filename that is unique (VideoConverted object's uuid),
+    then feeds the file to the ffmpeg input functions, then converts the video
+    according to its destination format with proper codecs
+    :param raw_uuid: uuid of the VideoRaw object created in view (as passed by the task)
+    :param conv_uuid: uuid of the VideoConverted object created in view (as passed by the task)
+    :return:
+    """
+
     raw_obj = VideoRaw.objects.get(uuid=raw_uuid)
     conv_obj = VideoConverted.objects.get(uuid=conv_uuid)
     req_format = raw_obj.req_format
-    output_filename = f"{raw_obj.filename[:-4]}.{req_format}"
+    output_filename = f"{conv_uuid}.{req_format}"
     stream = ffmpeg.input(raw_obj.file.path)
     if req_format == "3gp":
         stream = ffmpeg.output(
             stream,
-            f"{raw_obj.filename[:-4]}.{req_format}",
+            output_filename,
             vcodec="h263",
             acodec="aac",
             s="704x576"
@@ -35,5 +49,11 @@ def convert(raw_uuid, conv_uuid):
             output_filename,
             vcodec="mpeg4"
         )
-    print(ffmpeg.run(stream))
-    conv_obj.file = output_filename
+    ffmpeg.run(stream)
+    # copy the new file to media inside "converted_videos" directory, and then delete the file (here, the conversion
+    # process has finished and the raw file is not needed anymore.)
+    shutil.copy2(output_filename, f"{settings.MEDIA_ROOT}/converted_videos/{output_filename}")
+    os.remove(raw_obj.file.path)
+    os.remove(output_filename)
+    conv_obj.file = f"converted_videos/{output_filename}"
+    conv_obj.save()
