1c3d8b45f9c06b476e399ca875da1a58391a4a9e
hossein <mirjalali.hosein@gmail.com>
2021-12-29 17:24:17 +0330

    added django-cleanup package to delete files on object deletion; expired deletion task

--- converter/video/tasks.py
+++ converter/video/tasks.py
@@ -20,15 +20,13 @@ def convert_video_task(raw_uuid: uuid, conv_uuid: uuid, username: str):
     video_length /= 60  # divide by 60 for seconds
     user.convert_min_left -= video_length  # deduct from user's total remaining charge
     user.save()
-    pass
 
 
 @shared_task
 def delete_expired_converted_video_task():
     """
     query VideoConverted model and delete those objects that are expired
-    :return:
+    (files are also deleted because of django-cleanup package)
     """
     now = datetime.datetime.now()
     VideoConverted.objects.filter(expiration_time__lte=now).delete()
-    pass
